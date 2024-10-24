4b77a85f05c95bdd9cbc699d7d2196143ce9c540
OndrejKocak <ondrej.kocak@gmail.com>
2024-10-24 20:04:16 +0200

    added golang install to installDependencies.sh

--- installDependencies.sh
+++ installDependencies.sh
@@ -1,1 +1,19 @@
-sudo apt install -y texlive-full xxd
+#!/bin/bash
+add_to_bashrc=false
+sudo apt install -y texlive-full xxd wget
+wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz
+sudo rm go1.23.2.linux-amd64.tar.gz
+while getopts "f" opt; do
+  case $opt in
+    f)
+      echo "Not adding to bashrc"
+      ;;
+    \?)
+      echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+      source ~/.bashrc
+      ;;
+  esac
+done
+
+go version
