02a77c8e6c50ffa809264636fc5fb860d522f9fb
MatusKuma <xkuma@stuba.sk>
2024-11-07 17:29:32 +0100

    CRUD operations added

--- database/repository/test_repository.go
+++ database/repository/test_repository.go
@@ -9,3 +9,28 @@ func CreateTest(db *gorm.DB, test *models.Test) error {
 	result := db.Create(test)
 	return result.Error
 }
+
+func GetTest(db *gorm.DB, id uint) (*models.Test, error) {
+	var test models.Test
+	result := db.First(&test, id)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &test, nil
+}
+
+func GetAllTests(db *gorm.DB) ([]models.Test, error) {
+	var tests []models.Test
+	result := db.Find(&tests)
+	return tests, result.Error
+}
+
+func UpdateTest(db *gorm.DB, test *models.Test) error {
+	result := db.Save(test)
+	return result.Error
+}
+
+func DeleteTest(db *gorm.DB, id uint) error {
+	result := db.Delete(&models.Test{}, id)
+	return result.Error
+}
