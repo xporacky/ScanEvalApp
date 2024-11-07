dc12c56b0d28ce5489e11da2c69517ddb04f81c1
OndrejKocak <ondrej.kocak@gmail.com>
2024-11-07 11:43:17 +0100

    install dependencies fix

--- installDependencies.sh
+++ installDependencies.sh
@@ -4,16 +4,19 @@ sudo apt install -y texlive-full xxd wget
 wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz
 sudo rm go1.23.2.linux-amd64.tar.gz
+adding=true
 while getopts "f" opt; do
   case $opt in
     f)
+      addingx=false 
       echo "Not adding to bashrc"
-      ;;
-    \?)
-      echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-      source ~/.bashrc
       ;;
   esac
 done
+if [ "$adding" = true ] ; then
+      echo "Adding to bashrc"
+      echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.bashrc
+      exec $SHELL 
+fi
 
 go version
