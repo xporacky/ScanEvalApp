2eec307a44adb89b39bbd16f4d8dd8b8fec87e93
OndrejKocak <ondrej.kocak@gmail.com>
2024-11-20 20:32:15 +0100

    DeleteFile()

--- internal/files/files.go
+++ internal/files/files.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-// OpenFile načíta obsah súboru a vráti ho ako []byte
+// OpenFile load file from specified path
 func OpenFile(filePath string) ([]byte, error) {
 	data, err := os.ReadFile(filePath)
 	if err != nil {
@@ -14,11 +14,25 @@ func OpenFile(filePath string) ([]byte, error) {
 	return data, nil
 }
 
-// SaveFile uloží obsah []byte do súboru na špecifikovanej ceste
+// SaveFile saves []byte data to specified path
 func SaveFile(filePath string, data []byte) error {
 	err := os.WriteFile(filePath, data, 0644)
 	if err != nil {
 		return fmt.Errorf("chyba pri ukladaní súboru: %v", err)
+	}
+	return nil
+}
+
+// DeleteFile deletes file from specified path
+func DeleteFile(filePath string) error {
+	if _, err := os.Stat(filePath); err == nil {
+		// File exists, attempt to remove it
+		err = os.Remove(filePath)
+		if err != nil {
+			fmt.Println("Error while deleting file:", err)
+			return err
+		}
+		fmt.Println("Successfully deleted file:", filePath)
 	}
 	return nil
 }
