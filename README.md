457b7f45111a3ffadb3a1583a5f3ddffcbed32b4
OndrejKocak <ondrej.kocak@gmail.com>
2024-11-20 21:43:15 +0100

    fix in go installation path [dependencies installation]

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Dokumentácia dostupná [TU](https://docs.google.com/document/d/1oPEVyG-Ius-a9JK
 
 ### Install goCV
 `sudo apt update && sudo apt install build-essential cmake git pkg-config libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev gfortran openexr libatlas-base-dev python3-dev python3-numpy libtbbmalloc2 libtbb-dev`
-`cd /usr/local/bin/go/src && sudo git clone https://github.com/hybridgroup/gocv.git`
+`cd /usr/local/go/src && sudo git clone https://github.com/hybridgroup/gocv.git`
 `cd gocv && make install`
 
 #### Verify goCV installation
--- installDependencies.sh
+++ installDependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 add_to_bashrc=false
 sudo apt update
-sudo apt install -y texlive-full xxd wget gcc sqlite3 tesseract-ocr tesseract-ocr-slk
+sudo apt install -y texlive-full xxd wget gcc sqlite3 tesseract-ocr tesseract-ocr-slk 
 sudo apt install -y build-essential cmake git pkg-config libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev gfortran openexr libatlas-base-dev python3-dev python3-numpy libtbbmalloc2 libtbb-dev
 # go instalation
 wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
@@ -18,8 +18,7 @@ while getopts "f" opt; do
 done
 
 #goCV installation
-cd /usr/local/bin/go/src
-sudo git clone https://github.com/hybridgroup/gocv.git
+cd /usr/local/go/src && sudo git clone https://github.com/hybridgroup/gocv.git
 cd gocv
 make install
 go run ./cmd/version/main.go
