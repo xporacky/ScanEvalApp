0d423055a23dd04b51455ad8a88e199f3b555a80
Timotej Masár <timo.masar@gmail.com>
2024-11-23 00:43:52 +0100

    updated LaTeX template header

--- main.go
+++ main.go
@@ -1,33 +1,21 @@
 package main
 
-import "ScanEvalApp/internal/scanprocessing"
+import (
+	"ScanEvalApp/internal/files"
+	"ScanEvalApp/internal/latex"
+	"ScanEvalApp/internal/scanprocessing"
+	"fmt"
+)
 
 func main() {
 	// Načítanie LaTeX súboru a jeho otvorenie
-	/*
-		latexFilePath := "./assets/latex/main.tex"
-		latexContent, err := files.OpenFile(latexFilePath)
-		if err != nil {
-			fmt.Println("Error while opening LaTeX file:", err)
-			return
-		}
-	*/
-	// // Compile LaTeX to PDF
-	// pdfBytes, err := CompileLatexToPDF(latexContent)
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
 
-	// // Uloženie PDF
-	// outputFilePath := "./tmp/output.pdf"
-	// err = SaveFile(outputFilePath, pdfBytes)
-	// if err != nil {
-	// 	fmt.Println("Chyba pri ukladaní PDF súboru:", err)
-	// 	return
-	// }
-
-	// fmt.Println("PDF úspešne vytvorený a uložený ako:", outputFilePath)
+	latexFilePath := "./assets/latex/main.tex"
+	latexContent, err := files.OpenFile(latexFilePath)
+	if err != nil {
+		fmt.Println("Error while opening LaTeX file:", err)
+		return
+	}
 
 	//	db, err := migrations.MigrateDB()
 	//	if err != nil {
@@ -39,9 +27,8 @@ func main() {
 	//	fmt.Println("Database setup and seeding complete.")
 
 	// LaTeX generation pdf test
-
 	// Hodnoty na nahradenie kvôli testovaniu funkcionality
-	data := TemplateData{
+	data := latex.TemplateData{
 		ID:        "120345",
 		Meno:      "Jožko Alexander Mrkvička",
 		Datum:     "15. 11. 2024",
@@ -50,40 +37,30 @@ func main() {
 		Bloky:     50,
 		QrCode:    "www.google.com",
 	}
-	/*
-		data := latex.TemplateData{
-			ID:        "120345",
-			Meno:      "Jožko Alexander Mrkvička",
-			Datum:     "15. 11. 2024",
-			Miestnost: "CD300",
-			Cas:       "10:30",
-			Bloky:     50,
-		}
 
-		// Nahradenie placeholderov
-		updatedLatex, err := latex.ReplaceTemplatePlaceholders(latexContent, data)
-		if err != nil {
-			fmt.Println("Error replacing placeholders:", err)
-			return
-		}
+	// Nahradenie placeholderov
+	updatedLatex, err := latex.ReplaceTemplatePlaceholders(latexContent, data)
+	if err != nil {
+		fmt.Println("Error replacing placeholders:", err)
+		return
+	}
 
-		// Kompilácia upraveného LaTeX na PDF
-		pdfBytes, err := latex.CompileLatexToPDF(updatedLatex)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
+	// Kompilácia upraveného LaTeX na PDF
+	pdfBytes, err := latex.CompileLatexToPDF(updatedLatex)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-		// Uloženie výsledného PDF
-		outputFilePath := "./assets/tmp/output.pdf"
-		err = files.SaveFile(outputFilePath, pdfBytes)
-		if err != nil {
-			fmt.Println("Chyba pri ukladaní PDF súboru:", err)
-			return
-		}
+	// Uloženie výsledného PDF
+	outputFilePath := "./assets/tmp/output.pdf"
+	err = files.SaveFile(outputFilePath, pdfBytes)
+	if err != nil {
+		fmt.Println("Chyba pri ukladaní PDF súboru:", err)
+		return
+	}
 
-		fmt.Println("PDF úspešne vytvorený a uložený ako:", outputFilePath)
-	*/
+	fmt.Println("PDF úspešne vytvorený a uložený ako:", outputFilePath)
 
 	scanprocessing.ProcessPDF("assets/tmp/scan-pdfs", "assets/tmp/scan-images")
 }
