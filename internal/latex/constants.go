7341370e046e9112853dd20106ce3fef94da4659
Timotej Masár <timo.masar@gmail.com>
2025-01-27 19:02:22 +0100

    Added file for constants in latex backend

---
+++ internal/latex/constants.go
@@ -0,0 +1,11 @@
+package latex
+
+const (
+	//
+	FilePermission = 0644
+
+	// Cesty
+	TemplatePath     = "./assets/latex/main.tex"
+	OutputPDFPath    = "./assets/tmp/final.pdf"
+	TemporaryPDFPath = "./assets/tmp"
+)
