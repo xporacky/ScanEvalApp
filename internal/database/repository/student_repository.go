df3c23f67128331d884b0babf064967ab34d0a3a
RadoslavKubalec <xkubalec@stuba.sk>
2025-01-29 22:25:09 +0100

    filter student TODO

--- internal/database/repository/student_repository.go
+++ internal/database/repository/student_repository.go
@@ -4,6 +4,7 @@ import (
 	"ScanEvalApp/internal/database/models"
 
 	"gorm.io/gorm"
+	"fmt"
 )
 
 func CreateStudent(db *gorm.DB, student *models.Student) error {
@@ -34,4 +35,16 @@ func UpdateStudent(db *gorm.DB, student *models.Student) error {
 func DeleteStudent(db *gorm.DB, id uint) error {
 	result := db.Delete(&models.Student{}, id)
 	return result.Error
+}
+
+func GetStudentsQuery(db *gorm.DB, query string) ([]models.Student, error) {
+	//TODO: diakritika 
+	var students []models.Student
+
+	// Ak je query nenulové, filtrujeme podľa mena, priezviska a registračného čísla
+	result := db.Where("name LIKE ? OR surname LIKE ? OR registration_number LIKE ?", "%"+query+"%", "%"+query+"%", "%"+query+"%").Find(&students)
+
+	fmt.Println("studenti query:", query) // Opravené na správne formátovanie výstupu
+	return students, result.Error
+	
 }
