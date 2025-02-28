223485332e939719646052ce10d5c8ef5b67b1d8
matus kuma <matus.kuma@nuaktiv.sk>
2025-02-28 13:37:48 +0100

    testy - vyhodnotenie scanov

--- tests/ocr_test.go
+++ tests/ocr_test.go
@@ -165,8 +165,114 @@ var expectedCorrectedResults = map[int]map[int]string{
 	},
 }
 
+var expectedAnswer_1_page = map[int]string{
+	392411: "bccdeeddccdddcxcxceddcxebxcceddbeababddc",
+	913602: "aaaaabbbbbcccccdddddbcbcdbcdbcbababxxxxx",
+	985335: "abdcxbcdcdbxxaedxdcbabccbcbcdecbabcccbbc",
+	257457: "abdcaxdeccbcxaebbxecabcccccccbdcbadxaccd",
+	133168: "abcdeacdedxbdcbcdcdcbcdedcbacedcbabdedcb",
+	526606: "abcbacbcdcbcdbcbdxcxbbbbbxbbbxbbbcdxcxbc",
+	411098: "bdbcdbaaaabbbbbcccccdacdbcxdxdcxdcbdbxce",
+	631788: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	433835: "bedcdcdeccbeadcxxxxxaaabeabbbcbcdecaaaaa",
+	300532: "abcccbbcccbbbbbdddddeeeeeeeeeedddddaaaaa",
+	801650: "abbbbddddddcxabadcdbaaaaabbbbbcccccbbbbb",
+	783424: "abccccccccbbbbbacbcdabbbbccccceeeeeaaaaa",
+	990337: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+	753491: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+	648037: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+	188319: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	988443: "xxxxxxxxxxxxxxcxxxxxabcdexxxxxxxxxxxxxxx",
+	624245: "abcdeeeeeeeeeeeeeeeexdxxxexdeeeeeeeadxed",
+	774776: "xdaaaaaaaaaaabbbcdeeabcddeeeeeeeeeeeeeee",
+	236273: "aaaaaaaaaaaaaxaaaaaaaaaaaaaaaaaaaaaaaaaa",
+	537282: "cccccccccccccccccccccccccccccccccccccccc",
+	639863: "cccccccccccccccccccccccccccccccccccccccc",
+	227633: "cccccccccccccccccccccccccccccccccccccccc",
+	872413: "cccccccccccccccccccccccccccccccccccccccc",
+	212971: "abcxcdxcxcdeddcecdedaxcdebbddedddedxcccd",
+	507113: "xcccxxbdddddddccccxbcccccccccccccccccccc",
+	152342: "dddddddddddddddddddddddddddddddddddddddd",
+	870991: "abbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+	783456: "ccccccccccccccccccccdddcccbbbbbbbaxxxdec",
+	590787: "bbbbbbbbbbbbbbbbbbbbcccccccccccccccccccc",
+	705932: "abababcbcbcdcdcdededabababcbcbcdcdcdeded",
+	363580: "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde",
+	168030: "bdcaabccccdcccccccccedcbaabcdedcbaaabxde",
+	646042: "bdecacdecccbdccxxbccaaaaaaaaaaaaaaaaaaaa",
+	375942: "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde",
+	658013: "bcdecbcdedbcdddbbbbbbbbbbcbbbbbbbbbbbbbb",
+	798399: "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde",
+	984996: "abcdebcdeeabcdeabcdeabcdeabcdeabcdeabcde",
+	484470: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxaaaxxxxxxxx",
+	731579: "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde",
+	235850: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+	404065: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	760018: "ccccccccccccccccccccxxxxxbccdddddddddddd",
+	422417: "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde",
+	636975: "bbbbbbbbbbbbbbbbbbbbxxxxxxxxxxxxxxxxxxxx",
+	567742: "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde",
+	123800: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	385676: "bbbbbbbbbbbbbbbbbbbbbbbbbcccccbbbbbaaaaa",
+	761336: "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde",
+	110198: "dabcdcccccdeexdabcdeabcdeabcdeedcbbabxed",	
+}
+
+var expectedAnswer_both_page = map[int]string{
+	507113: "babababababababababababababababababababx",	
+	227633: "xxxxccccccccccccccccxxxxxxxxxxxxxxxxxccc",	
+	152342: "cccccccccccccccccccccccccccccccccccccccc",	
+	870991: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",	
+	590787: "dddddddddddddddddddddddddddddddddddddddd",	
+	646042: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",	
+	984996: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",	
+	404065: "dddddddddddddddddddddddddddddddddddddddd",	
+	567742: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",	
+	385676: "abbcxabcdexxxxxabcdeabcdeabcdeabxxeabcde",	
+	774776: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",	
+	375942: "aaaxxxxbbbbbbbbbbbbbxxxxxxxbxxxxxxbxxbxx",	
+	433835: "dddddddddddddddddddddddddddddddddddddddd",	
+	411098: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",	
+	526606: "bbbbbbbbbbbbbbbbbbbbccccccccccccccbbbbbb",	
+	257457: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",	
+	783456: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",	
+	801650: "ebccbcexcdxecbdbbbcdaaaaaadcbccdaabccccc",	
+	753491: "cebdaababcaaaaabcccdecbcdabcdcbbbbbccddd",	
+	110198: "aaaaaccccceeeeedbdbdaabcdcbbcdbbbbbccccc",	
+	705932: "bbbbbbbbbbbbbbbbbbbbxxxxxxxxxxxxxxxxxxxx",	
+	537282: "abcdeeeeeedcbaeedcbaabcdedcbabcdeedcbabc",	
+	484470: "eeeeeeeeeeeeeeeeeeeeabcdeedcbaabcdeedcba",	
+	422417: "abcdeeedcbabcdedcbaxxbcxeeeeeeeeeeeeeeee",	
+	761336: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",	
+	392411: "xxxxxxxxxxxxxxxxxxxxdedeeeeeeeeeeeeeeeee",	
+	212971: "abcdeeeddedeeeexxeeexxxxxxxxxxxxxxxxxxxx",	
+	783424: "abcdeedcbabcdedcbabcbbbbbdccccxddddxaaaa",	
+	133168: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",	
+	648037: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",	
+	872413: "abcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",	
+	639863: "abcddaaaaabbbbbdddddbabcdabcdeccaaabbbbb",	
+	363580: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",	
+	985335: "ceececdxcdcadddcddcbcebedcdeedcbabddcdee",	
+	168030: "eeeeecccccbbbbbxxxxxcccccbbbbbaaaaabcccc",	
+	235850: "abcdeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",	
+	798399: "aaaaabbbbbcccccdddddaaaaaeeeeedddddbbbbb",	
+	731579: "bbbbbbbaaabbababbbbbeeeeeeeeeedddddddddd",	
+	760018: "aaaaabbbbbcccccdddddbbbbbcccccdddddccccc",	
+	636975: "abbbcbbcbcbaaaaccdccaaaaabbbbbcdaaeabcbc",	
+	658013: "cccccccccccccccccccccccccccccccccccccccc",	
+	631788: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",	
+	990337: "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde",	
+	236273: "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde",	
+	300532: "abcdedcdedcbabcdedcbabcdedcbabcdedcbabcd",	
+	988443: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",	
+	123800: "aaaaabbbbbccccccccccaaaaacccccdddddbcdcb",	
+	624245: "aaaaabcdeababcdabcdeaabdcabcdcabcdcabbbb",	
+	913602: "abcdeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",	
+	188319: "bbbbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaa",	
+}
+
 func setupTestDB() (*gorm.DB, error) {
-	testDBPath := "../internal/database/scan-eval-test-db.db"
+	testDBPath := "../internal/database/scan-eval-db.db"
 	db, err := gorm.Open(sqlite.Open(testDBPath), &gorm.Config{})
 	if err != nil {
 		return nil, err
@@ -185,31 +291,32 @@ func TestAnswerRecognition(t *testing.T) {
 	totalQuestions := 0
 	totalCorrect := 0
 	totalMissing := 0
-	totalUnrecognized := 0 // New counter for unrecognized answers
+	totalUnrecognized := 0 
 
-	for studentID, expectedAnswers := range expectedResults {
-		student, err := repository.GetStudentById(db, uint(studentID), 1) // testID = 1
+	for studentID, expectedAnswers := range expectedAnswer_both_page {
+		student, err := repository.GetStudentByRegistrationNumber(db, uint(studentID), 1) // testID = 1
 		if err != nil {
-			t.Errorf("Študent %d nebol nájdený: %v", studentID, err)
-			totalMissing += 40
+			t.Errorf("Študent %d nebol nájdený: %v\n", studentID, err)
+			totalMissing += len(expectedAnswers)
 			continue
 		}
-
+		fmt.Printf("-----------------------\n")
 		recognizedAnswers := student.Answers
+		//ak nie je nic v DB
 		if len(recognizedAnswers) == 0 {
-			t.Errorf("Študent %d: chýbajúce odpovede", studentID)
-			totalMissing += 40
+			t.Errorf("Študent %d: chýbajúce odpovede\n", studentID)
+			totalMissing += len(expectedAnswers)
 			continue
 		}
 
 		correctCount := 0
 		missingCount := 0
 		unrecognized := 0
-		totalQuestions += 40
+		totalQuestions += len(expectedAnswers)
 
-		for i := 0; i < 40; i++ {
+		for i := 0; i < len(expectedAnswers); i++ {
 			if i >= len(recognizedAnswers) {
-				t.Errorf("Študent %d, otázka %d: chýbajúca odpoveď", studentID, i+1)
+				t.Errorf("Študent %d, otázka %d: chýbajúca odpoveď\n", studentID, i+1)
 				missingCount++
 				continue
 			}
@@ -219,17 +326,17 @@ func TestAnswerRecognition(t *testing.T) {
 			} else if recognizedAnswers[i] == '0' { // Check for unrecognized answer
 				totalUnrecognized++
 				unrecognized++
+				missingCount++
+			}else {
+				unrecognized++
+				fmt.Printf("Študent %d: Otázka č. %d, očakávané %s, rozpoznané %s\n", studentID, i+1, string(expectedAnswers[i]), string(recognizedAnswers[i]))
 			}
-			//t.Errorf("Študent %d, otázka %d: OCR nezachytilo odpoveď", studentID, i+1)
-			// } else {
-			// 	t.Errorf("Študent %d, otázka %d: očakávané %c, rozpoznané %c",
-			// 		studentID, i+1, expectedAnswers[i], recognizedAnswers[i])
-			// }
+			
 		}
 
 		totalCorrect += correctCount
 		totalMissing += missingCount
-		fmt.Printf("Študent %d: správne %d/40, chýbajúce %d, nezachytené %d\n", studentID, correctCount, missingCount, unrecognized)
+		fmt.Printf("Študent %d: správne %d/40, nesprávne %d, chýbajúce %d\n", studentID, correctCount, unrecognized, missingCount)
 
 	}
 
@@ -287,12 +394,12 @@ func TestMissingPages(t *testing.T) {
 
 		recognizedAnswers := student.Answers
 
-		// Počítaj počet znakov '0' v odpovediach
+		
 		zeroCount := strings.Count(recognizedAnswers, "0")
 
-		// Ak je počet znakov '0' 20 alebo viac, znamená to, že chýba aspoň jedna strana
+		
 		if zeroCount == 40 {
-			missingPages += 2 // Zaznamenaj jednu chýbajúcu stranu
+			missingPages += 2 
 		}
 		if zeroCount == 20 {
 			missingPages++
@@ -302,49 +409,4 @@ func TestMissingPages(t *testing.T) {
 	fmt.Printf("Celkový počet chýbajúcich strán: %d\n", missingPages)
 }
 
-func TestCorrectedAnswerRecognition(t *testing.T) {
-	errorLogger := logging.GetErrorLogger()
-	db, err := setupTestDB()
-	if err != nil {
-		errorLogger.Error("Nepodarilo sa pripojiť k databáze", slog.Group("CRITICAL", slog.String("error", err.Error())))
-		t.Fatalf("Nepodarilo sa pripojiť k databáze: %v", err)
-	}
 
-	totalCorrect := 0
-	totalQuestions := 0
-	totalUnrecognized := 0
-
-	for studentID, expectedAnswers := range expectedCorrectedResults {
-		student, err := repository.GetStudentById(db, uint(studentID), 1) // testID = 1
-		if err != nil {
-			t.Errorf("Študent %d nebol nájdený: %v", studentID, err)
-			continue
-		}
-
-		recognizedAnswers := student.Answers
-		if len(recognizedAnswers) == 0 {
-			t.Errorf("Študent %d: chýbajúce odpovede", studentID)
-			totalUnrecognized += len(expectedAnswers)
-			continue
-		}
-
-		for questionID, expectedAnswer := range expectedAnswers {
-			totalQuestions++
-
-			recognizedAnswer := string(recognizedAnswers[questionID-1]) // Convert byte to string
-
-			if recognizedAnswer == expectedAnswer {
-				totalCorrect++
-			} else if recognizedAnswer == "0" { // Check for unrecognized answer
-				totalUnrecognized++
-				t.Errorf("Študent %d, otázka %d: OCR nezachytilo odpoveď", studentID, questionID)
-			} else {
-				t.Errorf("Študent %d, otázka %d: očakávané %s, rozpoznané %s", studentID, questionID, expectedAnswer, recognizedAnswer)
-			}
-		}
-	}
-
-	successRate := float64(totalCorrect) / float64(totalQuestions) * 100
-	fmt.Printf("Celková úspešnosť rozpoznania opravených odpovedí: %.2f%% (%d/%d správnych odpovedí, %d nezachytených)\n",
-		successRate, totalCorrect, totalQuestions, totalUnrecognized)
-}
