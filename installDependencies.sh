3fc327152bf7299943ae0039e53a394857a8621a
Ondrej Kocak <44442630+OndrejKocak@users.noreply.github.com>
2025-05-01 10:27:38 +0200

    Update installDependencies.sh

--- installDependencies.sh
+++ installDependencies.sh
@@ -6,7 +6,7 @@ if grep -iq 'fedora' /etc/os-release; then
   sudo dnf update -y
   sudo dnf install -y texlive-scheme-medium xxd wget gcc g++ unzip sqlite3 tesseract tesseract-langpack-slk 
   sudo dnf install -y build-essential cmake git pkg-config libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev gfortran openexr libatlas-base-dev python3-dev python3-numpy libtbbmalloc2 libtbb-dev
-  sudo dnf install -y mesa-libEGL-devel libxkbcommon-devel wayland-devel vulkan-headers vulkan-loader-devel libxkbcommon-x11-devel pdftk
+  sudo dnf install -y mesa-libEGL-devel libxkbcommon-devel wayland-devel vulkan-headers vulkan-loader-devel libxkbcommon-x11-devel pdftk libx11-xcb-dev poppler-utils
   echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64/' >> ~/.bashrc
   source ~/.bashrc
 else
