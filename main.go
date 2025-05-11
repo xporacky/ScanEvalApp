8c6328baa669f749151dc71cb75f01781a5c0559
Timotej Masár <timo.masar@gmail.com>
2025-05-11 22:19:28 +0200

    refactor: main.go cleanup, add generated PDFs from app

--- main.go
+++ main.go
@@ -2,71 +2,12 @@ package main
 
 import (
 	"ScanEvalApp/internal/database/migrations"
-	"ScanEvalApp/internal/database/seed"
-
-	//"ScanEvalApp/internal/files"
-	// "ScanEvalApp/internal/latex"
-	//"ScanEvalApp/internal/scanprocessing"
 	window "ScanEvalApp/internal/gui"
 	"ScanEvalApp/internal/logging"
-
-	"gioui.org/app"
-
-	// "fmt"
-	//"time"
-
 	"log/slog"
 
-	"gorm.io/gorm"
+	"gioui.org/app"
 )
-
-// Kontrola ci je databaza prazdna
-func CheckIfDatabaseIsEmpty(db *gorm.DB) (bool, error) {
-	logger := logging.GetLogger()
-	errorLogger := logging.GetErrorLogger()
-
-	var count int64
-
-	err := db.Table("students").Count(&count).Error
-	if err != nil {
-		errorLogger.Error("Chyba pri kontrole databázy", slog.String("error", err.Error()))
-		return false, err
-	}
-	if count == 0 {
-		logger.Warn("Databáza je prázdna.")
-	} else {
-		logger.Info("Databáza obsahuje záznamy.", slog.Int64("count", count))
-	}
-	return count == 0, nil
-}
-
-// pomocna funkcia, ktora robi seedovanie, pokial mame prazdnu databazu (kvoli testovaniu generovania pdf pre studentov)
-func testDatabase(questionsCount int, studentsCount int) {
-	logger := logging.GetLogger()
-	errorLogger := logging.GetErrorLogger()
-
-	logger.Debug("Inicializujem databázu na testovanie.")
-
-	db, err := migrations.MigrateDB()
-	if err != nil {
-		errorLogger.Error("Nepodarilo sa pripojiť k databáze", slog.Group("CRITICAL", slog.String("error", err.Error()))) // Použi Error namiesto Critical
-		panic("failed to connect to database")
-	}
-
-	isEmpty, err := CheckIfDatabaseIsEmpty(db)
-	if err != nil {
-		errorLogger.Error("Chyba pri kontrole prázdnosti databázy", slog.String("error", err.Error()))
-		return
-	}
-
-	if isEmpty {
-		logger.Info("Databáza je prázdna, spúšťam seedovanie.")
-		seed.Seed(db, questionsCount, studentsCount)
-		logger.Info("Seedovanie dokončené.")
-	} else {
-		logger.Info("Databáza nie je prázdna, seedovanie preskočené.")
-	}
-}
 
 func main() {
 	logging.InitLogger()
@@ -78,51 +19,16 @@ func main() {
 
 	logger.Info("Aplikácia spustená")
 
-	var questionsCount int = 40 // pocet otazok ktore pridelujeme do testu, na testovanie
-	var studentsCount int = 50
-
 	// inicializacia a migracia db
 	logger.Info("Spúšťam migráciu databázy.")
 	db, err := migrations.MigrateDB()
 	if err != nil {
-		errorLogger.Error("Nepodarilo sa pripojiť k databáze", slog.Group("CRITICAL", slog.String("error", err.Error()))) // Použi Error namiesto Critical
+		errorLogger.Error("Nepodarilo sa pripojiť k databáze", slog.Group("CRITICAL", slog.String("error", err.Error())))
 		panic("failed to connect to database")
 	}
 	logger.Info("Migrácia databázy dokončená.")
 
-	// kontrola ci je prazdna databaza, kvoli seedovaniu
-	testDatabase(questionsCount, studentsCount)
-	/*
-			// Cesta k LaTeX sablone a cesta kam sa ma ulozit finalne pdf (pdf so studentami)
-			// zakomentovane, lebo je to v constants.go
-			// templatePath := "./assets/latex/main.tex"
-			// outputPDFPath := "./assets/tmp/final.pdf"
-
-			// Generovanie PDF pre vsetkych studentov
-			if err := latex.ParallelGeneratePDFs(db, latex.TemplatePath, latex.OutputPDFPath); err != nil {
-				fmt.Println("Chyba pri generovaní PDF:", err)
-				return
-			}
-
-		students, err := repository.GetAllStudents(db)
-		if err != nil {
-			return
-		}
-		for _, student := range students {
-			student.Answers = seed.GenerateAnswers(40)
-			repository.UpdateStudent(db, &student)
-		}
-		start := time.Now()
-		test, err := repository.GetExam(db, 1)
-		if err != nil {
-			return
-		}
-		scanprocessing.ProcessPDF("assets/tmp/scan-pdfs/Scan_20022025125623.PDF", test, db)
-		elapsed := time.Since(start)
-		fmt.Printf("Function took %s\n", elapsed)
-	*/
 	logger.Info("Spúšťam GUI.")
-	go window.RunWindow(db) // Zavolanie funkcie na vytvorenie a správu okna
+	go window.RunWindow(db)
 	app.Main()
-
 }
